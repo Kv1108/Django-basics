5dc5d2e2a6022345191941f27e29fdce992ba0e0
Kv1108 <Kv1108@users.noreply.github.com>
2025-12-30 17:16:37 +0530

    completed todo app in django

--- todo/todo_main/views.py
+++ todo/todo_main/views.py
@@ -1,10 +1,13 @@
 
 from django.shortcuts import render
 from todo.models import Task
+from django.http import HttpResponse
 
 def home(request):
     task = Task.objects.filter(is_completed=False).order_by('updated_at')
+    completed_task = Task.objects.filter(is_completed=True).order_by('updated_at')
     context = {
         'task': task,
+        'completed_task': completed_task,
     }
     return render(request, 'home.html', context)
