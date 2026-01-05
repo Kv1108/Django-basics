44e152251b5b3f01bc3948006e96842168770d8f
Kv1108 <Kv1108@users.noreply.github.com>
2026-01-05 12:20:58 +0530

    learnt comment in django template code

--- movie_club/members/views.py
+++ movie_club/members/views.py
@@ -25,11 +25,6 @@ def main(request):
 def testing(request):
   template = loader.get_template('template.html')
   context = {
-    'greeting': 1,
+    'fruits': ['Apple', 'Banana', 'Cherry', 'Oranges', 'Kiwi'],   
   }
-#   mymembers = Member.objects.all().values()
-#   template = loader.get_template('template.html')
-#   context = {
-#     'mymembers': mymembers,
-#   }
-#   return HttpResponse(template.render(context, request))
+  return HttpResponse(template.render(context, request))             
