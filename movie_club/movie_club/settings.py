5c43082d4708c9702a9fe4ddbdfb6d8b2c8bcd69
Kv1108 <Kv1108@users.noreply.github.com>
2026-01-05 12:06:13 +0530

    learnt about django extended templates, syntax of if and for

--- movie_club/movie_club/settings.py
+++ movie_club/movie_club/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-om-n&(*rbn@tx-za(xsgpdbpafz_-63bqjkg$3ycnb=sy71wg4'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1','localhost']
 
 
 # Application definition
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'movie_club.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / 'templates'],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
